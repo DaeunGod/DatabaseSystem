59b6e68ccd4bad1723fd9f522fc61e9e7035c826
DaeunGod <dcj4655@naver.com>
2019-06-03 15:37:55 +0900

    working directory changed

---
+++ DBprj#3_20131612/DBprj#3_20131612.py
@@ -0,0 +1,103 @@
+#-*- coding: utf-8 -*-
+
+import datetime
+import time
+import sys
+import MeCab
+import operator
+from pymongo import MongoClient
+from bson import ObjectId
+from itertools import combinations
+
+
+stop_word = {}
+DBname = "db20131612"
+conn = MongoClient("dbpurple.sogang.ac.kr")
+db = conn[DBname]
+db.authenticate(DBname, DBname)
+
+def printMenu():
+    print "0. CopyData"
+    print "1. Morph"
+    print "2. print morphs"
+    print "3. print wordset"
+    print "4. frequent item set"
+    print "5. association rule"
+
+def p0():
+    col1 = db['news']
+    col2 = db['news_freq']
+
+    col2.drop()
+
+    for doc in col1.find():
+        contentDic = {}
+        for key in doc.keys():
+            if key != "_id":
+                contentDic[key] = doc[key]
+        col2.insert(contentDic)
+
+def p1():
+    for doc in db['news_freq'].find():
+        doc['morph'] = morphing(doc['content'])
+        db['news_freq'].update({"_id":doc['_id']}, doc)
+
+def p2(url):
+    pass
+def p3():
+    pass
+def p4(url):
+    pass
+def p5(length):
+    pass
+def p6(length):
+    pass
+
+def make_stop_word():
+    f = open("wordList.txt", 'r')
+    while True:
+        line = f.readline()
+        if not line: break
+        stop_word[line.strip('\n')] = line.strip('\n')
+    f.close()
+
+def morphing(content):
+    t = MeCab.Tagger('-d/usr/local/lib/mecab/dic/mecab-ko-dic')
+    nodes = t.parseToNode(content.encode('utf-8'))
+    MorpList = []
+    while nodes:
+        if nodes.feature[0] == 'N' and nodes.featrue[1] == 'N':
+            w = nodes.surface
+            if not w in stop_word:
+                try:
+                    w = w.encode('utf-8')
+                    MorpList.append(w)
+                except:
+                    pass
+        nodes = nodes.next
+    return MorpList
+
+
+
+
+if __name__ == "__main__":
+    make_stop_word()
+    printMenu()
+    selector = input()
+    if selector == 0:
+        p0()
+    elif selector == 1:
+        p1()
+        p3()
+    elif selector == 2:
+        url = str(raw_input("input news url:"))
+        p2(url)
+    elif selector == 3:
+        url = str(raw_input("input news url:"))
+        p4(url)
+    elif selector == 4:
+        length = int(raw_input("input length of the frequent item:"))
+        p5(length)
+    elif selector == 5:
+        length = int(raw_input("input length of the frequent item:"))
+        p6(length)
